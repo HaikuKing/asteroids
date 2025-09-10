28c5c6b7eafd2d4505ef08864746a4012cb8bb2c
HaikuKing <haikuking6301@gmail.com>
2025-09-10 00:33:36 -0500

    player can shoot, destroy and split asteroids, and lose

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # type: ignore
     clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    Player.containers = (updatable, drawable)
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # type: ignore
 
     dt = 0
 
@@ -29,10 +32,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         updatable.update(dt)
+
+        for rock in asteroids:
+            for bullet in shots:
+                if rock.collision(bullet) == True:
+                    bullet.kill()
+                    rock.split()
+            if rock.collision(player) == True:
+                print("Game over!")
+                sys.exit()
+
         for object in drawable:
             object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
--- player.py
+++ player.py
@@ -1,39 +1,52 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 
-class Player(CircleShape): # type: ignore
+class Player(CircleShape): 
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS) # type: ignore
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation) # type: ignore
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5 # type: ignore
+        forward = pygame.Vector2(0, 1).rotate(self.rotation) 
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5 
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2) # type: ignore
+        pygame.draw.polygon(screen, "white", self.triangle(), 2) 
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt # type: ignore
 
     def update(self, dt):
-        keys = pygame.key.get_pressed() # type: ignore
+        self.timer -= dt
+        keys = pygame.key.get_pressed() 
 
-        if keys[pygame.K_a]: # type: ignore
+        if keys[pygame.K_a]: 
             self.rotate(-dt)
-        if keys[pygame.K_d]: # type: ignore
+        if keys[pygame.K_d]: 
             self.rotate(dt)
-        if keys[pygame.K_w]: # type: ignore
+        if keys[pygame.K_w]: 
             self.move(dt)
-        if keys[pygame.K_s]: # type: ignore
+        if keys[pygame.K_s]: 
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation) # type: ignore
+        forward = pygame.Vector2(0, 1).rotate(self.rotation) 
         self.position += forward * PLAYER_SPEED * dt # type: ignore
+
+    def shoot(self):
+        if self.timer > 0:
+            pass
+        else:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS) # type: ignore
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED # type: ignore
+            self.timer = PLAYER_SHOOT_COOLDOWN # type: ignore
